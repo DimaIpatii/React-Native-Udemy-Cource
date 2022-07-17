95c23aee4cf896a0c2e8548c5cdce39635209d59
Dimitrii <32913446+DimaIpatii@users.noreply.github.com>
2022-07-17 20:22:57 +0200

    Reducer config

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import MainScreen from "./app/screens/MainScreen";
+import GameScreen from "./app/screens/GameScreen";
+
+import { Provider } from "react-redux";
+import store from "./app/store/store";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!!</Text>
+      <Provider store={store}>
+        <Text>Hello World!!</Text>
+        <MainScreen />
+        <GameScreen />
+      </Provider>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
